91e1c3dab5313e24ea0a38dcc8b8b9ce213f6cba
Z L <lizhaoshi@gmail.com>
2025-07-04 22:51:23 -0400

    development

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -10,16 +10,17 @@ const randomeIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css'
 })
 export class User {
-  @Input({ required: true}) id!: string;
-  @Input({ required: true}) avatar!: string;
-  @Input({ required: true}) name!: string;
+  @Input({ required: true}) user!: {
+    id: string;
+    avatar: string;
+    name: string
+  }
+
   @Output() select = new EventEmitter();
-  // select = output<string>();
 
-
-  get imagePath() {return 'assets/users/' + this.avatar;}
+  get imagePath() {return 'assets/users/' + this.user.avatar;}
 
   onSelectUser(){   
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
