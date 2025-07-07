899e67133ec69f92e031f9db31bb3841c757159a
Z L <lizhaoshi@gmail.com>
2025-07-07 16:14:36 -0400

    localstorage

--- src/app/tasks/tasks.service2.ts
+++ src/app/tasks/tasks.service2.ts
@@ -6,6 +6,14 @@ import { NewTaskData } from "./task/task.model";
 export class TasksService{
     private  tasks = dummyTasks;
     
+
+    constructor(){
+        const tasks = localStorage.getItem('tasks');
+        if(tasks) {
+            this.tasks = JSON.parse(tasks);
+        }
+    }
+
     getUserTask(userId: string){
         return this.tasks.filter((task) => task.userId === userId );
     }
@@ -17,11 +25,17 @@ export class TasksService{
           title: taskData.title,
           summary: taskData.summary,
           dueDate: taskData.date
-        })
+        });
+        this.saveTasks();
     }    
 
     removeTask(userId: string){
         this.tasks = this.tasks.filter((task) => task.id !== userId);
+        this.saveTasks();
+    }
+
+    private saveTasks(){
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
 
 }
